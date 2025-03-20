cfb2dc090addbea69037a2846ab21f64e8da199f
Yuxuan Ma <yuxuanm4@illinois.edu>
2025-03-19 22:27:58 -0500

    feat: support HTTP/1.1 headers and file URLs
    - Updated HTTP request to use HTTP/1.1 instead of HTTP/1.0.
    - Added "Connection: close" and "User-Agent" headers, using a headers dictionary for easy future modifications.
    - Added support for file URLs (file:///) to load local files.
    - Implemented a default file (test.html) to open when no URL is provided.

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import socket
 import ssl
+import os
 
 class URL:
     def __init__(self, url):
         self.scheme, url = url.split("://", 1)
-        assert self.scheme in ["http", "https"]
+        assert self.scheme in ["http", "https", "file"]
+
+        if self.scheme == "file":
+            # For file URLs, treat the remainder as a file path.
+            # If the path isn't absolute (i.e. doesn't start with '/'), add a leading slash.
+            if not url.startswith("/"):
+                url = "/" + url
+            self.path = url
+            return
 
         if self.scheme == "http":
             self.port = 80
@@ -17,16 +26,20 @@ class URL:
         self.host, url = url.split("/", 1)
         self.path = "/" + url
 
-        # if ports are given
+        # if ports are given in the host (e.g. example.com:8080)
         if ":" in self.host:
             self.host, port = self.host.split(":", 1)
             self.port = int(port)
 
-
     def request(self):
+        # Handle file scheme separately.
+        if self.scheme == "file":
+            with open(self.path, "r", encoding="utf-8") as f:
+                return f.read()
+
         s = socket.socket(
-            family = socket.AF_INET,
-            type = socket.SOCK_STREAM,
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
             proto=socket.IPPROTO_TCP,
         )
         s.connect((self.host, self.port))
@@ -35,23 +48,30 @@ class URL:
             ctx = ssl.create_default_context()
             s = ctx.wrap_socket(s, server_hostname=self.host)
 
-        request = "GET {} HTTP/1.0\r\n".format(self.path)
-        request += "Host: {}\r\n".format(self.host)
+        # Build headers dictionary for easy future modifications.
+        headers = {
+            "Host": self.host,
+            "Connection": "close",
+            "User-Agent": "MySimpleBrowser/1.0",
+        }
+        # Construct the HTTP request using HTTP/1.1.
+        request = "GET {} HTTP/1.1\r\n".format(self.path)
+        for key, value in headers.items():
+            request += "{}: {}\r\n".format(key, value)
         request += "\r\n"
+
         s.send(request.encode("utf-8"))
 
-        # makefile returns a file-like object containing every byte we receive from the server.
         response = s.makefile("r", encoding="utf-8", newline="\r\n")
         statusline = response.readline()
-        # split the response into pieces
         version, status, explanation = statusline.split(" ", 2)
 
         response_headers = {}
         while True:
             line = response.readline()
-            if line == "\r\n": break
+            if line == "\r\n":
+                break
             header, value = line.split(": ", 1)
-            # normalize to lower case and strip off whitespace
             response_headers[header.casefold()] = value.strip()
 
         assert "transfer-encoding" not in response_headers
@@ -73,17 +93,18 @@ def show(body):
         elif not in_tag:
             print(c, end="")
 
+
 def load(url):
     body = url.request()
     show(body)
 
+
 if __name__ == "__main__":
     import sys
-    load(URL(sys.argv[1]))
-
-
-
-
-
-
-
+    if len(sys.argv) < 2:
+        # If no URL is provided, open a default file (e.g., test.html in the current directory)
+        default_file = os.path.abspath("test.html")
+        url = "file://" + default_file
+    else:
+        url = sys.argv[1]
+    load(URL(url))
